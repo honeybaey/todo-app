2cbf06d620f2301515728b07e3f78fe84eba1ba7
honeybaey <honeybaey@gmail.com>
2020-09-05 18:56:06 +0300

    adding a task deletion function

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,13 +2,14 @@ import React from "react";
 import TodoListItem from "../TodoListItem/TodoListItem";
 import "./TodoList.css";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
   const elements = todos.map((item) => {
     const { id, ...itemProps } = item;
 
     return (
       <li key={id} className="list-group-item">
-        <TodoListItem {...itemProps} /> {/* this is spread operator */}
+        <TodoListItem {...itemProps} onDeleted={() => onDeleted(id)} />{" "}
+        {/* this is spread operator */}
         {/* <TodoListItem label={item.label} important={item.important} /> */}
       </li>
     );
