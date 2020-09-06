0fac851aa8dbb2ef514f39aea936c49ecfbc2cbc
honeybaey <honeybaey@gmail.com>
2020-09-06 19:17:58 +0300

    added a function for quickly adding an item prop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -53,6 +53,14 @@ export default class App extends Component {
         };
       });
     };
+    this.toggleProperty = (arr, id, propName) => {
+      const idx = arr.findIndex((el) => el.id === id);
+
+      const oldItem = arr[idx];
+      const newItem = { ...oldItem, [propName]: !oldItem[propName] };
+
+      return [...arr.slice(0, idx), newItem, ...arr.slice(idx + 1)];
+    };
   }
 
   render() {
