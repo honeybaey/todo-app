404fec0211a8d266875f6ef07ba1a1a32c897e04
honeybaey <honeybaey@gmail.com>
2020-09-07 19:22:59 +0300

    added feature for adding a new task

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -3,16 +3,40 @@ import React, { Component } from "react";
 import "./ItemAddForm.css";
 
 export default class ItemAddForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      label: "",
+    };
+
+    this.onLabelChange = (e) => {
+      this.setState({
+        label: e.target.value,
+      });
+    };
+
+    this.onSubmit = (e) => {
+      e.preventDefault();
+      this.props.onAddItem(this.state.label);
+      this.setState({
+        label: "",
+      });
+    };
+  }
+
   render() {
     return (
-      <div className="item-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onAddItem("Hello")}
-        >
-          Добавить задачу
-        </button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="Введите название задачи"
+          value={this.state.label}
+        />
+        <button className="btn btn-outline-secondary">Добавить</button>
+      </form>
     );
   }
 }
