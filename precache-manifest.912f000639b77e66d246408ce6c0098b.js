63cb6a1ae190b7fdc69efd428fbf0aefcafa8cea
honeybaey <honeybaey@gmail.com>
2020-09-08 15:40:44 +0300

    Updates

---
+++ precache-manifest.912f000639b77e66d246408ce6c0098b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f684747837fda685f29f944c2bc2ae2e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ee88c93f8c32d7292ee4",
+    "url": "/todo/static/css/main.3311ba3c.chunk.css"
+  },
+  {
+    "revision": "75c422e360a043139e1e",
+    "url": "/todo/static/js/2.b71df67b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.b71df67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee88c93f8c32d7292ee4",
+    "url": "/todo/static/js/main.4eab0535.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
